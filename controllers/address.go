91d7175784b166b401867aeb0a265f8d53614e4c
Hruday Chowdary Gurijala <gurijalahruday@gmail.com>
2025-07-07 23:41:46 +0530

    mongodb agg to get items from cart

--- controllers/address.go
+++ controllers/address.go
@@ -1,20 +1,60 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"net/http"
+	"time"
 
-//add address
-func AddAddress() gin.HandlerFunc{
-	
-}
-//edit home address
-func EditHomeAddress() gin.HandlerFunc{
+	"github.com/HrudayGurijala/ecommerce-go/models"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// add address
+func AddAddress() gin.HandlerFunc {
 
 }
-//edit work address
-func AddWorkddress() gin.HandlerFunc{
+
+// edit home address
+func EditHomeAddress() gin.HandlerFunc {
 
 }
-//delete address
-func DeleteAddress() gin.HandlerFunc{
-	
+
+// edit work address
+func AddWorkddress() gin.HandlerFunc {
+
+}
+
+// delete address
+func DeleteAddress() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		user_id := c.Query("id")
+		if user_id == "" {
+			c.Header("Content-Type", "application/json")
+			c.JSON(http.StatusNotFound, gin.H{"Error": "Invalid Search Index"})
+			c.Abort()
+			return
+		}
+		addresses := make([]models.Address, 0)
+		usert_id, err := primitive.ObjectIDFromHex(user_id)
+		if err != nil {
+			c.IndentedJSON(500, "Internal Server Error")
+		}
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+
+		filter := bson.D{primitive.E{Key: "_id", Value: usert_id}}
+		update := bson.D{{Key: "$set", Value: bson.D{primitive.E{Key: "address", Value: addresses}}}}
+		_, err = UserCollection.UpdateOne(ctx, filter, update)
+		if err != nil {
+			c.IndentedJSON(404, "Wromg")
+			return
+		}
+		
+		defer cancel()
+		ctx.Done()
+		c.IndentedJSON(200, "Successfully Deleted!")
+	}
 }
